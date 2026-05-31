ddad2705f5fc81d44a5c1fc25cc5e1678ab49b73
nanashino gonbe <84581538+jinDoe1107@users.noreply.github.com>
2026-05-31 19:21:27 +0900

    ブラウザのタブアイコンの修正

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -10,6 +10,11 @@ export default defineNuxtConfig({
       ],
       link: [
         {
+          rel: 'icon',
+          type: 'image/svg+xml',
+          href: '/favicon.svg',
+        },
+        {
           rel: 'preconnect',
           href: 'https://fonts.googleapis.com',
         },
